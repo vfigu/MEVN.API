f72e599675834e728e7f2ebcda55bcfb5154a379
vfigu <vicente.figueroa.950@my.csun.edu>
2018-12-18 22:55:53 -0800

    Removed unused lists models; socket.io set up but could not achieve connection

--- models/chats.js
+++ models/chats.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 let ChatSchema = new Schema({
-  name: String,
-  room : {
-    type: Schema.Types.ObjectId,
-    ref: 'lists'
-  },
+  room: String,
+  username: String,
+  message: String,
   date_created: {
     type: Date,
     default: Date.now
   },
-  message: String,
 });
 
 module.exports = mongoose.model('chats', ChatSchema);
--- routes/chats.js
+++ routes/chats.js
@@ -1,34 +1,55 @@
 const express = require('express');
 const router = express.Router();
 const chats = require('../models/chats.js');
-const authorization = require('../middlewares/authorization');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('../node_modules/socket.io')(server);
+const port = 8000;
+// const authorization = require('../middlewares/authorization');
+//
+//
+// router.use(authorization);
 
+console.log('Listening for connections on port ' + port);
 
-router.use(authorization);
+io.on('connection', function(socket) {
+    console.log('Socket connected');
+    socket.emit('fromServer', {id: port}); // send message fromServer to client
+
+    socket.on('fromClient', function(data) { // listen for fromClient message
+        console.log('Connected to Client ' + data.id);
+        socket.emit('Handshake'); // send message fromServer to client
+    });
+
+    socket.on('Handshake', function() { // listen for fromServer message
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Client disconnected');
+    });
+});
 
 /* GET ALL chats */
 router.get('/', function(req, res, next) {
-  chats.find(function (err, products) {
+  chats.find(req.body, function (err, products) {
     if (err) return next(err);
     res.json(products);
-  });
+  }).sort({$natural:-1}).limit(50);
 });
 
-/* GET chat by id */
-router.get('/:id', function(req, res, next) {
-  chats.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
+// /* GET chat by id */
+// router.get('/:id', function(req, res, next) {
+//   chats.findById(req.params.id, function (err, post) {
+//     if (err) return next(err);
+//     res.json(post);
+//   });
+// });
 
 /* Create chat */
 router.post('/', function(req, res, next) {
   chats.create(req.body, function (err, post) {
     if (err) return next(err);
+    res.json(post);
   });
 });
 
@@ -36,6 +57,7 @@ router.post('/', function(req, res, next) {
 router.put('/:id', function(req, res, next) {
   chats.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
+    res.json(post);
   });
 });
 
@@ -43,6 +65,7 @@ router.put('/:id', function(req, res, next) {
 router.delete('/:id', function(req, res, next) {
   chats.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
+    res.json(post);
   });
 });
 
